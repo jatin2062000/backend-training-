50bcb4fffbf8d3f782c78ee5c7efe56b6451231e
jatinkumar2062000 <jatinkumar2062000@gmail.com>
2022-08-06 00:24:38 +0530

    my 1st node.js assignmnet

---
+++ src/logger/logger.js
@@ -0,0 +1,9 @@
+const myanme = "jatin kumar"
+
+let welcome = function() {
+    console.log('Welcome to my application. I am ' +myanme+' and a part of FunctionUp Plutonium cohort.')
+
+}
+
+module.exports.name = myanme
+module.exports.welcome = welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,9 @@
+function problem(){
+var problem1 = "       jatin kumar         "
+var problem2 = "JATIN KUMAR"
+var problem3 = "jatin kumar"
+console.log(problem1.trim())
+console.log(problem2.toLowerCase())
+console.log(problem3.toUpperCase())
+} 
+module.exports.problem = problem
