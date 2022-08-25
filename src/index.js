0591e786b5d604e97bd730bbc551c8a28733ac93
jatinkumar2062000 <jatinkumar2062000@gmail.com>
2022-08-25 15:50:49 +0530

    new obj

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://jatinkumar0007:zWDyt4ENYWMMhjva@cluster0.a0k73vb.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -20,7 +20,26 @@ app.use('/', route);
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        // console.log ("inside GLOBAL MW");
+        let obj={
+         "name":"jatin",
+         "city":"delhi",
+         "address_pincode":"110045",
+        }
+        obj.age=12             // agr hum bhr bhi bhi likh kr print kr sakte hai toh hum bhr bhi write kr skate hai
+        obj["age"]=12
+
+
+        console.log(obj.name)
+        console.log(obj["name"])
+        console.log(obj.city)
+        console.log(obj["city"])
+        console.log(obj.address_pincode)
+        console.log(obj["city"],obj["address_pincode"])
+        console.log(obj.age)
+        console.log(obj["age"])
+       
+
         res.send({msg:"done"})
   }
   );
