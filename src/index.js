491e318d0330876ed978c162585f9d32b64f1b04
jatinkumar2062000 <jatinkumar2062000@gmail.com>
2022-08-30 14:36:22 +0530

    hello

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+var bodyParser = require('body-parser');
 const { default: mongoose } = require('mongoose');
+const route = require('./routes/route.js');
+const moment = require('moment');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
+mongoose.connect("mongodb+srv://jatinkumar0007:zWDyt4ENYWMMhjva@cluster0.a0k73vb.mongodb.net/test", 
+{useNewUrlParser: true})
 .then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+.catch ( err => console.log(err) );
 
-app.use('/', route)
+// app.use(
+//     function (req,res,next){
+//         console.log(moment().format('YYYY-MM-DD HH:mm:ss'),',',req.ip,',',req.path);
+//         next();
+//     }
+// );
 
+app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const mw=require("../middleware/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser)
+router.post("/users", userController.newUser)
 
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.get("/users/:userId",mw.Authenticate , mw.authorise, userController.getUserDetails )
+router.post("/users/:userId/posts",mw.Authenticate , mw.authorise, userController.postMessage)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.put("/users/:userId",mw.Authenticate , mw.authorise, userController.updateUserDetails)
+router.delete('/users/:userId',mw.Authenticate , mw.authorise, userController.deleteUser)
 
 module.exports = router;
