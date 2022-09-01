af0bc28de52b8077265c9a4768a230df4fffb323
jatinkumar2062000 <jatinkumar2062000@gmail.com>
2022-09-01 13:41:17 +0530

    hello trya dn catch

---
+++ src/middlewares/auth.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+const Authenticate = function (req, res, next) {
+  try {
+    let reqHeaders = req.headers;
+    if (reqHeaders["x-auth-token"]) next();
+    else {
+      res.send({
+        status: false,
+        msg: "Request is missing a mandatory HEADER!",
+      });
+    }
+  } catch (err) {
+    res.status(500).send({ status: false, msg: err });
+  }
+};
+
+const authorise = function (req, res, next) {
+  try {
+    // comapre the logged in user's id and the id in request
+    let token = req.headers["x-Auth-token"];
+    if (!token) token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, "SecretKey");
+    if (decodedToken.userId != req.params.userId) {
+      next();
+    } else {
+      return res.send({
+        status: false,
+        msg: "You are not authorizre to do this task",
+      });
+    }
+  } catch (err) {
+    res.status(500).send({ status: false, msg: err });
+  }
+};
+
+module.exports = { Authenticate, authorise };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema(
+  {
     firstName: String,
     lastName: String,
     mobile: {
-        type: String,
-        unique: true,
-        required: true
+      type: String,
+
+      required: true,
     },
     emailId: String,
+    password: String,
     gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+      type: String,
+      enum: ["male", "female", "other"],
     },
     age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
-}, { timestamps: true });
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    posts: { type: [], deafult: [] },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema) //users
-
-
-
-// String, Number
-// Boolean, Object/json, array
+module.exports = mongoose.model("User", userSchema);
